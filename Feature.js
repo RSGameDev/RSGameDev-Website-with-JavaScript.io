6512f34fe4d905206fa15e6cd9bf8c3dd085563c
RSGameDev <rsgaming@hotmail.co.uk>
2024-08-08 16:50:25 +0100

    DarkMode+StickyMenu on all pages
    Amended files so the features occur across the website.

--- Feature.js
+++ Feature.js
@@ -1,6 +1,27 @@
 // JavaScript source code
 
+
+// Darkmode 
 function darkModeFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+// Sticky bar
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.getElementById("nav");
+    var sticky = navbar.offsetTop;
+
+    window.onscroll = function () {
+        stickyFunction(navbar, sticky);
+    };
+
+    function stickyFunction(navbar, sticky) {
+        if (window.scrollY >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+});
