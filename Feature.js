7475c07902f7221d745a3fbcd6fc2789a86d13f8
RSGameDev <rsgaming@hotmail.co.uk>
2024-08-09 09:40:30 +0100

    Full Screen Mode added
    For each webpage in the bottom right corner. An icon is visible where the user can click to make the webpage switch to full screen mode.

--- Feature.js
+++ Feature.js
@@ -31,3 +31,19 @@ function popUpFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+// Full Screen
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+var elem = document.documentElement;
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
